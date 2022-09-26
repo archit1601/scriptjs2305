d022851d8798d6c29522976b0e9318eb65430ebd
archit1601 <architsahu@cedcoss.com>
2022-09-26 13:19:14 +0530

    handsonJS

---
+++ script.js
@@ -0,0 +1,137 @@
+// js variables (excercise1)
+var carName = "Volvo";
+// js variables (excercise2)
+var x = 50;
+// js variables (excercise3)
+var x = 5;
+var y = 10;
+
+document.getElementById("demo").innerHTML = x + y;
+
+// js variables (excercise4)
+var x = 5;
+var y = 10;
+var c = x + y ;
+alert (z);
+
+// js variables (excercise5)
+var firstName = "john", lastName = "Doe", age = 35 ;
+
+// js opertaors (excercise1)
+alert(10*5);
+
+// js opertaors(excercise2)
+alert(10/2);
+
+// js opertaors(excercise3)
+alert(15%9);
+
+// js opertaors(excercise4)
+x =10;
+y =5;
+x+=y;
+
+// js  opertaors(excercise5)
+x = 5;
+y = 10;
+x *= y;
+
+//js data types excerciae 1
+let length = 15; //number data type
+let lastname = "johnson"; //string data type
+const x
+{
+    firstName: "John";
+    lastname: "Doe";  //oject data type 
+}
+
+//js functions excercise1
+function myFunction(){
+    alert("hello world");
+}
+myFunction;
+//js functions excercise2
+
+function myFunction(){
+    alert("hello world");
+}
+//js functions excercise3
+
+function myFunction() {
+    return ("Hello");
+   }
+   document.getElementById("demo").innerHTML = myFunction();
+
+//js functions excercise4
+Document.getElementByID("demo").innerHTML="Hello";
+
+//js objects excercie1
+const person = {
+    firstName: "John",
+    lastName: "Doe"
+  };
+  alert(person.firstName);
+//js objects excercie2
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    country: "Norway"
+};
+
+//js objects excercie3
+const person ={
+    name: "john",age:50,
+}
+alert (person.name + "is" + person.age);
+
+// js events excercise1
+<button 
+onclick
+="alert('Hello')">Click me.</button>
+// js events excercise2
+<button 
+onclick
+="
+myFunction()
+">Click me.</button>
+
+// js events excercise3
+<div 
+onmouseover
+="this.style.backgroundColor='red'">myDIV.</div>
+
+
+// js strings excercise1
+let txt = "Hello World!";
+let x = 
+txt.length
+;
+alert(x);
+// js strings excercise2
+let txt = "we are \"vikings\"";
+alert (txt);
+// js strings excercise3
+let str1 = "Hello ";
+let str2 = "World!";
+alert(strn1 + strn2);
+
+// js strings excercise1
+
+let txt = "Hello World!";
+txt = txt.toUpperCase();
+// js strings excercise2
+
+let txt = "I can eat bananas all day";
+txt = txt(10,17);
+// js strings excercise3
+let txt = "Hello World";
+txt = txt.replace("hello","welcome");
+// js strings excercise4
+let txt = "Hello World";
+txt = txt.toUpperCase();
+
+// js strings excercise5
+let txt = "Hello World";
+txt = txt.toLowerCase();
+
+
